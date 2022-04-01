c1261c5f22c66715bca8991d402001efd43508fa
plug010 <m.angel.rios@gmail.com>
2022-04-01 09:47:22 -0600

    Update datapipeline.py

--- datapipeline.py
+++ datapipeline.py
@@ -18,3 +18,8 @@ COPOMEX_API_KEYS='6b0f6b3f-6985-4f05-a300-8a4cca8a363a' #Not Used
 DB_username='miguel'
 DB_password='4531194169'
 
+#Connect to MongoDb Database
+client = pymongo.MongoClient("mongodb+srv://miguel:4531194169@serverlessinstance0.k8rxn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.metrobus
+buses =  db.buses
+postal_code = db.postal_code
