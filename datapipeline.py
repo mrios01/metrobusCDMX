df2f5452e037d0d0be65705457c344f78170c3a5
plug010 <m.angel.rios@gmail.com>
2022-04-01 09:33:57 -0600

    Connect to MongoDb  Atlas Database

--- datapipeline.py
+++ datapipeline.py
@@ -14,7 +14,7 @@ import unidecode
 #Declare Constants -> Access Tokens
 
 MAPS_API_KEYS='AIzaSyAabU45w1hEuoya-m_8EYHYbPoQ4RfMX10'
-COPOMEX_API_KEYS='6b0f6b3f-6985-4f05-a300-8a4cca8a363a'
+COPOMEX_API_KEYS='6b0f6b3f-6985-4f05-a300-8a4cca8a363a' #Not Used
 DB_username='miguel'
 DB_password='4531194169'
 
